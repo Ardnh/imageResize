31c2af07efd3166f062186bdc8f65ff937d31e53
Ardan Hilal <ardanhilal@pop-os.localdomain>
2023-02-22 20:11:00 +0700

    add disintergration/imaging & implement resize functioality

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	_ "image/jpeg"
+	"io/ioutil"
+
+	"github.com/disintegration/imaging"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	imagePath := "/home/ardanhilal/Documents/coding/golang/imageResize"
+	files, _ := ioutil.ReadDir(fmt.Sprintf("%s/%s", imagePath, "data"))
+	count := 0
+	for _, f := range files {
+		src, _ := imaging.Open(fmt.Sprintf("%s/%s/%s", imagePath, "data", f.Name()))
+
+		transformedImage := imaging.Resize(src, 320, 240, imaging.Lanczos)
+
+		imaging.Save(transformedImage, fmt.Sprintf("%s/saved/%s", imagePath, f.Name()))
+
+		count++
+		print := fmt.Sprintf("image with name %s has been saved", f.Name())
+		fmt.Println(print)
+	}
+
+	print(count)
 }
